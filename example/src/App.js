050a6da0f9619c04ed65a06dd5bb05bc11c65c80
calebdeji <calebdeji06@gmail.com>
2020-10-02 15:04:09 +0100

    rewrite app to test for code

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,33 @@
-import React from 'react'
-
-import { useMyHook } from 'use-network-status'
+import React, { useEffect } from 'react';
+import useNetworkStatus from 'use-network-status';
+import axios from 'axios';
 
 const App = () => {
-  const example = useMyHook()
-  return (
-    <div>
-      {example}
-    </div>
-  )
-}
-export default App
+	const { networkStatusCode, clearStatus } = useNetworkStatus({
+		urls: [
+			'https://ghibliapi.herokuapp.com/film',
+			'https://ghibliapi.herokuapp.com/filmsd'
+		]
+	});
+
+	useEffect(() => {
+		fetch('https://ghibliapi.herokuapp.com/film')
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				console.log({ dataNisin: data });
+			});
+
+		const id = setTimeout(async () => {
+			try {
+				await axios.get('https://ghibliapi.herokuapp.com/filmsd');
+			} catch (error) {
+				console.log('error ');
+			}
+		}, 4000);
+	}, []);
+
+	return <div>See this {JSON.stringify(networkStatusCode)} </div>;
+};
+export default App;
