e776dc9d648f15c8407a7de441da0430a57cffed
calebdeji <calebdeji06@gmail.com>
2020-11-10 06:08:46 +0100

    🎨 refactor: update example

--- example/src/App.jsx
+++ example/src/App.jsx
@@ -1,60 +1,50 @@
-import React, { useEffect } from 'react';
-import { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import useNetworkStatusCode from 'use-network-status-code';
 import axios from 'axios';
 
+const fetchResourceWithFetchAPI = (url) => {
+	return new Promise(async (resolve, reject) => {
+		fetch(url)
+			.then((res) => res.json())
+			.then((data) => resolve(data))
+			.catch((err) => reject(err));
+	});
+};
+
+const baseURLs = [
+	'https://ghibliapi.herokuapp.com/film',
+	'https://ghibliapi.herokuapp.com/gbedi'
+];
+
+const excludingURLs = [
+	'https://ghibliapi.herokuapp.com/login',
+	'https://ghibliapi.herokuapp.com/film/sd'
+];
+
 const App = () => {
 	const { networkStatusCode, clearStatus } = useNetworkStatusCode({
-		baseURLs: [
-			'https://ghibliapi.herokuapp.com/film',
-			'https://ghibliapi.herokuapp.com/gbedi'
-		],
-		excludingURLs: [
-			'https://ghibliapi.herokuapp.com/login',
-			'https://ghibliapi.herokuapp.com/film/sd'
-		]
+		baseURLs,
+		excludingURLs
 	});
 
 	let { current: id } = useRef(null);
-
-	console.log({ networkStatusCode });
 
 	const clear = () => {
 		clearStatus();
 	};
 
 	const clearGbediEndpoint = () => {
-		clearStatus('https://ghibliapi.herokuapp.com/gbedi');
+		clearStatus(baseURLs[1]);
 	};
 
 	const reload = () => {
-		fetch('https://ghibliapi.herokuapp.com/film')
-			.then((response) => {
-				return response.json();
-			})
-			.then((data) => {
-				console.log({ filmData: data });
-			});
-
-		fetch('https://ghibliapi.herokuapp.com/film/sd')
-			.then((response) => {
-				return response.json();
-			})
-			.then((data) => {
-				console.log({ filmSDData: data });
-			});
-
-		fetch('https://ghibliapi.herokuapp.com/gbedi')
-			.then((response) => {
-				return response.json();
-			})
-			.then((data) => {
-				console.log({ gbediData: data });
-			});
+		fetchResourceWithFetchAPI(baseURLs[0]);
+		fetchResourceWithFetchAPI(excludingURLs[1]);
+		fetchResourceWithFetchAPI(baseURLs[1]);
 
 		id = setTimeout(async () => {
 			try {
-				await axios.get('https://ghibliapi.herokuapp.com/login');
+				await axios.get(excludingURLs[0]);
 			} catch (error) {
 				console.log('error ');
 			}
